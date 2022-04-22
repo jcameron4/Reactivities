465048d2cfa1058e61dc8faca066abd64f8735ca
John Cameron <john_cameron@nwtc.com>
2022-04-22 13:47:52 -0400

    End of Section 2

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,20 @@
+using System;
+using Domain;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    
+    }
+
+}
